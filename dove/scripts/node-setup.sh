93034ce83ed5629a70215ed1de6224e10181e324
Said BOUDJELDA <bmscomp@gmail.com>
2025-12-12 00:21:09 +0100

    fix script for installing packages

--- dove/scripts/node-setup.sh
+++ dove/scripts/node-setup.sh
@@ -1,4 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+
+# Force apt to use the correct proxy
+echo 'Acquire::http::Proxy "http://host.docker.internal:9000";'  > /etc/apt/apt.conf.d/99proxy
+echo 'Acquire::https::Proxy "http://host.docker.internal:9000";' >> /etc/apt/apt.conf.d/99proxy
+
 apt-get update
 apt-get install -y \
   curl \
